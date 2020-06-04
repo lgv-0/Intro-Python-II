9a7aa55bb269029341ae51800af4ffda1184a655
Logan Van Hook <loganvan@att.net>
2020-06-04 14:48:40 -0400

    Work

--- src/adv.py
+++ src/adv.py
@@ -1,10 +1,12 @@
 from room import Room
+from player import Player
+from item import Item
 
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons", [ Item("Stick", "Seriously, a stick.") ]),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
@@ -39,6 +41,8 @@ room['treasure'].s_to = room['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 
+new_player = Player("Logan", room["outside"])
+
 # Write a loop that:
 #
 # * Prints the current room name
@@ -49,3 +53,72 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+while True:
+    current_room = new_player.current_room
+    print(current_room.name)
+    print(current_room.description)
+    print("Items Visible: ")
+    for item in current_room.items:
+        print(" - " + item.name + " -> " + item.description)
+
+    x = input("Specify an action: ")
+
+    x = x.split()
+
+    if x.__len__() == 1:
+        x = x[0]
+        if x is "q":
+            quit("Bye!")
+        elif x is "n":
+            if "n_to" not in vars(current_room):
+                print("Invalid direction")
+                continue
+
+            new_player.current_room = current_room.n_to
+        elif x is "s":
+            if "s_to" not in vars(current_room):
+                print("Invalid direction")
+                continue
+
+            new_player.current_room = current_room.s_to
+        elif x is "e":
+            if "e_to" not in vars(current_room):
+                print("Invalid direction")
+                continue
+
+            new_player.current_room = current_room.e_to
+        elif x is "w":
+            if "w_to" not in vars(current_room):
+                print("Invalid direction")
+                continue
+
+            new_player.current_room = current_room.w_to
+        
+        elif x in ["i", "inventory"]:
+            for item in new_player.items:
+                print(f"INV ITEM: {item.name}, {item.description}")
+    else:
+        if x[0] in ["get", "take"]:
+            found = False
+            for item in current_room.items:
+                if item.name == x[1]:
+                    current_room.items.remove(item)
+                    new_player.pickup(item)
+                    item.on_take()
+                    found = True
+
+            if found:
+                continue
+
+            print("Couldn't find that item...")
+        elif x[0] in ["drop"]:
+            targetItem = False
+            for item in new_player.items:
+                if item.name == x[1]:
+                    targetItem = item
+
+            if not targetItem:
+                continue
+                
+            current_room.items.append(targetItem)
+            new_player.items.remove(targetItem)
